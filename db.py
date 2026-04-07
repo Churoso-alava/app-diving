ad72e3b42b00124d831b52f06662c29395ee72ae
Churoso-alava <sbarbosaa@endeporte.edu.co>
2026-04-06 23:54:33 -0500

    Update db.py

--- db.py
+++ db.py
@@ -27,8 +27,8 @@ def get_client() -> Client:
         SUPABASE_URL  = "https://xxxx.supabase.co"
         SUPABASE_KEY  = "eyJ..."   # anon key (RLS activo)
     """
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
+    url = st.secrets["https://hbmoztlcbrfcayoiqmpy.supabase.co"]
+    key = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhibW96dGxjYnJmY2F5b2lxbXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUzMzI4ODEsImV4cCI6MjA5MDkwODg4MX0.FOlsrQXdBahHg1DsgvVJz43pDWXzRmXAaetPW_kVlH4"]
     return create_client(url, key)
 
 
