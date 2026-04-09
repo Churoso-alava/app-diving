864d6983ac1b12a55b32e7fe01b86303dab6b1a5
Churoso-alava <sbarbosaa@endeporte.edu.co>
2026-04-08 23:52:30 -0500

    Update __init__.py

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -1,39 +1,37 @@
 """
-   visualization package
-   Módulo de temas, gráficos y componentes UI para NMF-Optimizer
-   """
+visualization package - NMF-Optimizer
+"""
 
-   from .themes import (
-       COLORS,
-       STATUS_COLOR,
-       STATUS_EMOJI,
-       get_global_css,
-   )
-   from .charts import (
-       fig_vmp_tendencia,
-       fig_semaforo_barras,
-       fig_semaforo_historico,
-       fig_membership_fuzzy,
-   )
-   from .components import (
-       render_kpi_row,
-       render_athlete_bars,
-       render_athlete_profile,
-   )
+from visualization.themes import (
+    COLORS,
+    STATUS_COLOR,
+    STATUS_EMOJI,
+    get_global_css,
+)
 
-   __all__ = [
-       # themes
-       "COLORS",
-       "STATUS_COLOR",
-       "STATUS_EMOJI",
-       "get_global_css",
-       # charts
-       "fig_vmp_tendencia",
-       "fig_semaforo_barras",
-       "fig_semaforo_historico",
-       "fig_membership_fuzzy",
-       # components
-       "render_kpi_row",
-       "render_athlete_bars",
-       "render_athlete_profile",
-   ]
+from visualization.charts import (
+    fig_vmp_tendencia,
+    fig_semaforo_barras,
+    fig_semaforo_historico,
+    fig_membership_fuzzy,
+)
+
+from visualization.components import (
+    render_kpi_row,
+    render_athlete_bars,
+    render_athlete_profile,
+)
+
+__all__ = [
+    "COLORS",
+    "STATUS_COLOR",
+    "STATUS_EMOJI",
+    "get_global_css",
+    "fig_vmp_tendencia",
+    "fig_semaforo_barras",
+    "fig_semaforo_historico",
+    "fig_membership_fuzzy",
+    "render_kpi_row",
+    "render_athlete_bars",
+    "render_athlete_profile",
+]
